45308cf851b5e991f5f404aedf961616c7c83ba6
arya-sa <rizkyaryas11@gmail.com>
2023-08-13 14:50:14 +0700

    add Location Section to Homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Hero from "../components/sections/Hero";
-import Services from "../components/sections/Services";
-import Features from "../components/sections/Features";
-import Plans from "../components/sections/Plans";
+import Location from "../components/sections/Location";
+// import Services from "../components/sections/Services";
+// import Features from "../components/sections/Features";
+// import Plans from "../components/sections/Plans";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
+      <Location />
     </>
   );
 };
