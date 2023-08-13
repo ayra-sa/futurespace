f9ef3da17171ef86dcb7210435941f5edcb1f945
arya-sa <rizkyaryas11@gmail.com>
2023-08-13 14:45:15 +0700

    add dummy data

---
+++ src/data/data.ts
@@ -0,0 +1,137 @@
+export const locationData = {
+  image: "images/location-image.png",
+  label: "Our Offices",
+  title: "Discover the Location that Best Fits Your Business Needs",
+  description:
+    "As we headstart into this new normal, we are ensuring your workspace experience remains exceptional.",
+  locations: [
+    { location: "New Orleans, USA" },
+    { location: "Bangalore, India" },
+    { location: "Sydney, Australia" },
+  ],
+};
+
+export const reviewData = [
+  {
+    label: "reviews",
+    title: "What Our Members Say",
+    reviews: [
+      {
+        photo: "/image",
+        name: "Brittany",
+        work: "Co-Founder & CTO - Payfi",
+        review:
+          "Quisque consectetur vulputate odio, non dictum metus porttitor molestie. Duis pharetra suscipit dolor, quis euismod velit fringilla sed.",
+      },
+      {
+        photo: "/image",
+        name: "Randall Robertson",
+        work: "Project lead at Zoole",
+        review:
+          "Lorem ipsum dolor sit amet consectetur non adipiscing elit gravida posuere odio metus adipiscing tincidunt venenatis amet sagittis tellus porttitor enim blandit venenatis tellus.",
+      },
+      {
+        photo: "/image",
+        name: "Del Gibbs",
+        work: "VP, Globee Softech",
+        review:
+          "Futurspace is an excellent coworking space for my agency. It’s a great place to meet other business. owners expectations",
+      },
+    ],
+  },
+];
+
+export const footerData = [
+  {
+    logo: "/logo",
+    about:
+      "We want to build more than nice, shared office spaces. We want to build a community.",
+    socials: [
+      {
+        social: "Facebook",
+        icon: "/icon",
+        link: "#",
+      },
+      {
+        social: "Linkedin",
+        icon: "/icon",
+        link: "#",
+      },
+      {
+        social: "Instagram",
+        icon: "/icon",
+        link: "#",
+      },
+      {
+        social: "Twitter",
+        icon: "/icon",
+        link: "#",
+      },
+    ],
+    menus: [
+      {
+        label: "About",
+        menu: [
+          {
+            name: "About Us",
+            link: "#",
+          },
+          {
+            name: "Pricing",
+            link: "#",
+          },
+          {
+            name: "Offices",
+            link: "#",
+          },
+          {
+            name: "Spaces",
+            link: "#",
+          },
+        ],
+      },
+      {
+        label: "Product",
+        menu: [
+          {
+            name: "Office Space",
+            link: "#",
+          },
+          {
+            name: "Business Lounge",
+            link: "#",
+          },
+          {
+            name: "Meeting Room",
+            link: "#",
+          },
+          {
+            name: "Virtual Office",
+            link: "#",
+          },
+        ],
+      },
+      {
+        label: "Contact",
+        menu: [
+          {
+            name: "Help Desk",
+            link: "#",
+          },
+          {
+            name: "Book a Tour",
+            link: "#",
+          },
+          {
+            name: "Blog",
+            link: "#",
+          },
+          {
+            name: "Forum",
+            link: "#",
+          },
+        ],
+      },
+    ],
+  },
+];
