fd185bc260a83bf41c2108d352ccf69db7370225
arya-sa <rizkyaryas11@gmail.com>
2023-08-14 09:47:31 +0700

    add Cta Section

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "../components/sections/Hero";
 import Location from "../components/sections/Location";
 import Review from "../components/sections/Review";
+import Cta from "../components/sections/Cta";
 // import Services from "../components/sections/Services";
 // import Features from "../components/sections/Features";
 // import Plans from "../components/sections/Plans";
@@ -12,6 +13,7 @@ const HomePage = () => {
       <Hero />
       <Location />
       <Review />
+      <Cta />
     </>
   );
 };
