9036ef3f10d4825f2d9bfd7755f18119c43ffaf1
alfinabalqis <alfinabalqis28@gmail.com>
2023-08-15 15:05:54 +0700

    style: update mobile hero section about

--- src/components/sections/about/Hero.tsx
+++ src/components/sections/about/Hero.tsx
@@ -6,18 +6,17 @@ const Hero = () => {
     <section className="bg-primary py-8">
       <Container>
         <div className="flex justify-between items-center gap-x-8 box">
-         <div>
-         <h1 className="text-white">
-            About our <br /> company
-          </h1>
-         </div>
-          <div className="text-white max-w-[45%]">
-          <p>
-            Our space is more than a shared office. It is a meeting ground for
-            creatives and teams <br /> <br />
-            Find focus and clarity space to do the things you need to do to grow
-            your business in New York. Book by the month, daily or by the hour.
-          </p>
+          <div className="max-w-[20%] content-hero">
+            <h1 className="text-white text-center">About our company</h1>
+          </div>
+          <div className="text-white max-w-[45%] content-hero text-center lg:text-left">
+            <p>
+              Our space is more than a shared office. It is a meeting ground for
+              creatives and teams <br /> <br />
+              Find focus and clarity space to do the things you need to do to
+              grow your business in New York. Book by the month, daily or by the
+              hour.
+            </p>
           </div>
         </div>
         <div className="w-full pt-10">
